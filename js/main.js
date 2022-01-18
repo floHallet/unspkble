49d5e71af87bce0d970f64e9a82078a6e55a5bc9
floHal <donotreply@nothing.com>
2022-01-18 10:48:36 +0100

    Add jquery/smoothScrolling, mediaQueries

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-//smooth scrolling with js
-/*
-document.getElementById("test").onclick = function(event) {
-    event.preventDefault();
-    document.querySelector('#bio').scrollIntoView({ 
-        behavior: 'smooth' 
-      });
-    };
-*/
+$(document).ready(function() {
+/* SMOOTH SCROLLING WITH JQUERY 
+visit https://css-tricks.com/snippets/jquery/smooth-scrolling/
+----------------------------------------------------------- */
+// Select all links with hashes
+$('a[href*="#"]').click(function(event) {
+  // Figure out element to scroll to
+  const $target = $(this.hash);
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $target.offset().top
+  }, 800, function() {
+    // Callback after animation
+    // Must change focus?!
+    });
+  });
+});
