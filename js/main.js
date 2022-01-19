f27c7a27d4fe71147c03a2e01f9046ac0f8a4a57
floHal <donotreply@nothing.com>
2022-01-19 21:48:26 +0100

    ADD topArrow CHANGE Music Section

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@
 visite https://www.simonbattersby.com/blog/simple-jquery-image-crossfade/
 -----------------------------------------------------------------------*/
 function cycleImages(){
-  var $active = $('#cycler .active');
-  var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+  const $active = $('#cycler .active');
+  const $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
   $next.css('z-index',2);//move the next image up the pile
   $active.fadeOut(800,function(){//fade out the top image
     $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
     $next.css('z-index',3).addClass('active');//make the next image the top one
   });
+}
+
+function topPos(){
+  const pos = $(document).scrollTop();
+  if (pos <= 250) {
+    //$('.arrow').css('display', 'none');
+    $('.arrow').hide();
+  } else {
+    $('.arrow').show();
+  }
 }
 
 // START JQUERY CODE AFTER PAGE IS LOADED
@@ -28,7 +38,19 @@ $('a[href*="#"]').click(function(event) {
     // Must change focus?!
     });
   });
-});
+
+//check if user scroll
+/*$(document).scroll(function(){
+  $('.arrow').css('display', 'block'); // show arrow at the bottom of sreen
+  //$(document).off('scroll'); //turn off the event listener
+});*/
+
+setInterval('topPos()', 2000);
+
 
 // CHANGE ABOUT SECTION IMAGE EVERY 5S
 setInterval('cycleImages()', 5000);
+
+});
+
+
